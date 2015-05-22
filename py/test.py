7100ab16fb969db4a8d44ed2a75034aee781edeb
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-05-22 16:31:41 -0400

    functions to get the departure time

--- py/test.py
+++ py/test.py
@@ -4,6 +4,31 @@ import pandas as pd
 # trucks = pd.read_csv("./data/faf_trucks.csv")
 
 class TruckPlan:
+def get_start_day():
+    """
+    :return: a random day of the week. For now all days are the same,
+    but we don't have to make it that way. We have a two-week simulation
+    """
+    return np.random.randint(1, 14)
+
+def get_departure_time():
+    """
+    :return:
+    """
+    y0 = np.random.randn()
+    y1 = np.random.randn()
+    flag = np.random.binomial(1, 0.5)
+    y = y0 * (1 - flag) + y1 * flag
+    if y < 0:
+        # time cannot be less than midnight
+        y = np.random.randint(0, 6 * 3600)
+    elif y > 24 * 3600:
+        # or greater than midnight
+        y = np.random.randint(18 * 3600, 24 * 3600)
+    else:
+        y *= 3600
+    return int(y)
+
 
     'Critical information for the truck plan'
     truckCount = 0

ff9afc5ac783da49f89ddc7b8c4115f592a755b9
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-06-03 13:42:02 -0400

    Simulation to handle import and export nodes.

--- py/disaggregate_trucks.py
+++ py/disaggregate_trucks.py
@@ -5,7 +5,44 @@ import pandas as pd
 import lxml.etree as et
 import gzip
 import numpy as np
+def pick_ienode(dict_table, mode, zone):
+    """
+    :param dict_table: the dictionary table of import and export nodes.
+    :param mode: the import or export mode, indicating what facility (port,
+      airport, seaport) should be chosen.
+    :param zone: the FAF zone on the import or export end.
+    :return: the airport, seaport, or highway crossing
+    """
+    ienode = np.random.choice(
+        dict_table[zone][mode].keys(),
+        p=dict_table[zone][mode].values())
+    return ienode
 
+
+        # get the origin points ----
+        if self.inmode in ['1', '3', '4']:  # imported?
+            try:
+                # If a valid import node exists, use it
+                self.origin = pick_ienode(EXIM_DICT, self.inmode, self.origin)
+            except KeyError:
+                # If it doesn't, just assign like normal
+                self.get_origin()
+        else:
+            self.get_origin()
+
+        # get the destination points ----
+        if self.outmode in ['1', '3', '4']:  # imported?
+            try:
+                # If a valid import node exists, use it
+                self.destination = pick_ienode(EXIM_DICT,  self.outmode,
+                                               self.destination)
+            except KeyError:
+                # If it doesn't, just assign like normal
+                self.get_destination()
+        else:
+            self.get_destination()
+
+        self.write_plan()
 
 # Read in the I/O tables and convert them to dictionaries.
 MAKE_DICT = tp.recur_dictify(pd.read_csv(
@@ -28,7 +65,10 @@ west_coast_f3z = range(61, 69) + range(411, 419) + range(531, 539) + \
 
 # Exports/Imports are directed to airports, seaports, or highway border
 # crossings in the FAF zone.
-#EXIM_DICT =
+EXIM_DICT = recur_dictify(pd.read_csv(
+    "./data/ienodes.csv",
+    dtype={'F3Z': np.str, 'mode': np.str, 'name': np.str}
+))
 
 
 # Create the element tree container
@@ -38,12 +78,14 @@ pop_file = et.ElementTree(population)
 # read in the split trucks file with numbers of trucks going from i to j.
 faf_trucks = pd.read_csv("./data/faf_trucks.csv",
                          dtype={'dms_orig': np.str, 'dms_dest': np.str,
-                                'sctg': np.str, 'trucks': np.int})
-faf_trucks = faf_trucks.head(5)
+                                'sctg': np.str, 'trucks': np.int,
+                                'fr_inmode': np.str, 'fr_outmode': np.str})
+faf_trucks = faf_trucks[faf_trucks['fr_inmode'] == '1'][:5]
 
 # create the appropriate numbers of trucks for each row.
 for index, row in faf_trucks.iterrows():
-    [tp.TruckPlan(row['dms_orig'], row['dms_dest'], row['sctg'])
+    [TruckPlan(row['dms_orig'], row['dms_dest'], row['sctg'],
+                  row['fr_inmode'], row['fr_outmode']),
      for _ in range(row['trucks'])]
 
 with gzip.open('population.xml.gz', 'w', compresslevel=0) as f:

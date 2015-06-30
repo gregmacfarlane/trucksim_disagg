cdc015ae04cda789535688d406d003581dd92903
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-06-30 13:48:25 -0400

    Remove oil production from other industries.

--- R/prep_BEA.R
+++ R/prep_BEA.R
@@ -16,9 +16,14 @@ gdp <- read_csv(
   
   # only keep all industries and oil/gas for now.
   filter(Line %in% c(1, 7)) %>%
-  mutate(industry = c("all", "oil")) %>%
+  mutate(industry = c("other", "oil")) %>%
   gather(data_year, gdp, `2006`:`2013`, convert = TRUE) %>%
-  select(industry, data_year, gdp)
+  select(industry, data_year, gdp) %>%
+  
+  # remove oil from other industries
+  spread(industry, gdp) %>%
+  mutate(other = other - oil) %>%
+  gather(industry, gdp, oil:other)
  
 gdp <- gdp %>%
   # calculate percent change from 2007
@@ -32,7 +37,7 @@ gdp <- gdp %>%
 # join sctg codes
 sctg <- data_frame(
   sctg = sprintf("%02d", c(1:41, 43, 99)),
-  industry = ifelse(sctg %in% c("16", "17", "18", "19"), "oil", "all")
+  industry = ifelse(sctg %in% c("16", "17", "18", "19"), "oil", "other")
 )
 
 gdp <- gdp %>% 
